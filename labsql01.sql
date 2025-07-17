4285e04141c91ce25028f6a7f96c49ee5fa97978
nongpraw <nong.praw12345678@gmail.com>
2025-07-17 15:53:31 +0700

    Add files via upload

---
+++ labsql01.sql
@@ -0,0 +1,7 @@
+Select * from Products;
+Select * from Employees;
+SELECT EmployeeID , FirstName, LastName from Employees;
+SELECT * from Employees WHERE City = 'London';
+SELECT * from Categories;
+SELECT * from Customers;
+
