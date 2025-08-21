6f88818b811f837d93813a53623b67e64915b51a
nongpraw <nong.praw12345678@gmail.com>
2025-08-21 14:45:09 +0700

    Add files via upload

--- lab06.sql
+++ lab06.sql
@@ -32,3 +32,10 @@ ORDER BY 2 desc
 SELECT p.ProductID, p.ProductName, sum(Quantity)จำนวนที่ขายได้
 from Products p JOIN [Order Details] od on p.ProductID =od.ProductID
 GROUP BY p.ProductID, p.ProductName 
+
+SELECT Distinct  p.ProductID pProductName
+FROM Employees e JOIN Orders o on  e.EmployeeID = o.EmployeeID 
+                JOIN[Order Details] OD ON O.OrderID = od.OrderID
+                JOIN Products p on p.ProductID = od.ProductID
+WHERE e.FirstName = 'Nancy'           
+ ORDER BY p.ProductID
