85cf55c1b1dbd9496c74e4c764f1111ef6c67ff2
nongpraw <nong.praw12345678@gmail.com>
2025-08-21 14:12:59 +0700

    Add files via upload

--- lab06.sql
+++ lab06.sql
@@ -11,3 +11,11 @@ WHERE  CategoryName ='seafood'
 
 SELECT * from Orders WHERE OrderID =10250
 SELECT * FROM [Order Details] WHERE orderID = 10250
+
+SELECT p.ProductID, p.ProductName, s.CompanyName, s.Country
+from Products p join Suppliers s on p.SupplierID = s.SupplierID
+Where Country in ('usa','uk')
+
+SELECT e.EmployeeID, FirstName, o.OrderID
+from Employees e join Orders o on e.EmployeeID = o.EmployeeID
+order by EmployeeID
