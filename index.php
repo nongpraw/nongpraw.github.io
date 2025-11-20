0dde2157e50882c83261277d019a02d06d9ef624
nongpraw <nong.praw12345678@gmail.com>
2025-11-20 09:49:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello, world!";
+        echo "<br>";
+        echo "<h2>My Name is Phimchanok</h2>";
+    ?>
+</body>
+</html>
